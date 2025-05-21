12932112893642060136febece7a6c90007163d1
viml kumar <vimalkg@gmail.com>
2025-05-22 02:34:51 +0530

    hscii.vercel.app hfont.vercel.app mck82.vercel.app fb.com/ztrabc x.com/etphor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 //import Image from "next/image";
 import { Hsciifontpicker } from "@/components/hsciifp/Hsciifontpicker";
-import {MyClientComponent} from "libhsciifp/client";
+// import {Hsciifontpicker} from "libhsciifp/client";
 export default function Home() {
   const iplaceholer = `A=x≠a (xpple=Apple) a=>car cAt=cxt rxt=rAt=mouse=cuva fast
 H=v≠h  wveel=wHeel kvani=kHani=story 
@@ -24,7 +24,6 @@ a->ram i->sijaram u->vnuman e->gnesh o->gowinq
     <div className="w-full">
       <main className="flex flex-col gap-[4px] row-start-2 items-center sm:items-start">
         <Hsciifontpicker/>
-        <MyClientComponent/>
         <textarea id="ta1" rows={12} className="p-4 min-w-full sm:text-xl, md:text-2xl text-purple-700" placeholder={iplaceholer} />
         <a
             className="border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
--- src/components/hsciifp/Hsciifontpicker.tsx
+++ src/components/hsciifp/Hsciifontpicker.tsx
@@ -4,8 +4,7 @@ import fgioptions from "./fgioptions_sel1g2.json" ;
 import { hsciifont_classnames } from "./hsciifontsg4";
 
 export const Hsciifontpicker = () => {
-  const handle_fitem_change = (curr_selected_fitem: string) => { if (curr_selected_fitem) { setBodyFont(curr_selected_fitem); } }; 
-  const handle_fitem_change2 = (event: React.ChangeEvent<HTMLSelectElement>) => { if (event.target.value) { setBodyFont(event.target.value); } }; 
+  const handle_fitem_change = (event: React.ChangeEvent<HTMLSelectElement>) => { if (event.target.value) { setBodyFont(event.target.value); } }; 
   const setBodyFont = (selected_hsciifont_name:string) => {
     document.body.classList.value = document.body.classList.value.replaceAll(/__className_\w+\s+antialiased/g,"");
     document.body.classList.value = document.body.classList.value.replaceAll(/antialiased\s+__className_\w+/g,"");
@@ -14,12 +13,10 @@ export const Hsciifontpicker = () => {
   };
   return (
     <div>
-      <select  onChange={handle_fitem_change2} defaultValue={''} >
+      <select  onChange={handle_fitem_change} defaultValue={''} >
       <option value="" disabled>hscii_font select please</option>
       {fgioptions.map((option) => (
-        <option key={option.walue} value={option.walue}>
-          {option.label}
-        </option>
+        <option key={option.walue} value={option.walue}> {option.label} </option>
       ))}
     </select>
     </div>
