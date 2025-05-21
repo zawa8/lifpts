419c845f066268bd5c2108ba412b08e973508d97
viml kumar <vimalkg@gmail.com>
2025-05-22 01:45:10 +0530

    hscii.vercel.app mck82.vercel.app fb.com/ztrabc x.com/etphor

--- src/components/hsciifp/Hsciifontpicker.tsx
+++ src/components/hsciifp/Hsciifontpicker.tsx
@@ -1,13 +1,11 @@
 "use client" ;
 import React from "react";
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import fgioptions from "./fgioptions_sel1g2.json" ;
 import { hsciifont_classnames } from "./hsciifontsg4";
 
 export const Hsciifontpicker = () => {
-  const handle_fitem_change = (curr_selected_fitem: string) => {
-    if (curr_selected_fitem) { setBodyFont(curr_selected_fitem); }
-  }; 
+  const handle_fitem_change = (curr_selected_fitem: string) => { if (curr_selected_fitem) { setBodyFont(curr_selected_fitem); } }; 
+  const handle_fitem_change2 = (event: React.ChangeEvent<HTMLSelectElement>) => { if (event.target.value) { setBodyFont(event.target.value); } }; 
   const setBodyFont = (selected_hsciifont_name:string) => {
     document.body.classList.value = document.body.classList.value.replaceAll(/__className_\w+\s+antialiased/g,"");
     document.body.classList.value = document.body.classList.value.replaceAll(/antialiased\s+__className_\w+/g,"");
@@ -16,16 +14,14 @@ export const Hsciifontpicker = () => {
   };
   return (
     <div>
-      <Select onValueChange={handle_fitem_change}>
-        <SelectTrigger className="w-[180px] mt-4"> <SelectValue placeholder="hscii_font=>select"/> </SelectTrigger>
-        <SelectContent>
-        {fgioptions.map((fgioption) => (
-            <SelectItem key={fgioption.walue} value={fgioption.walue}>
-              {fgioption.label}
-            </SelectItem>
-          ))}
-        </SelectContent>
-      </Select>
+      <select  onChange={handle_fitem_change2} defaultValue={''} >
+      <option value="" disabled>hscii_font select please</option>
+      {fgioptions.map((option) => (
+        <option key={option.walue} value={option.walue}>
+          {option.label}
+        </option>
+      ))}
+    </select>
     </div>
   );
 };
